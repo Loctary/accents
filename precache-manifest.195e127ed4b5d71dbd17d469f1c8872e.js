f79b3ea510b327a8a1f02965a8bcc5d75ef9cae4
Vasyl Butov <loctary@mail.ru>
2019-07-29 17:50:19 +0300

    Updates

---
+++ precache-manifest.195e127ed4b5d71dbd17d469f1c8872e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f36c165352dde153ed6905cdd20b5c",
+    "url": "/accents/index.html"
+  },
+  {
+    "revision": "5fd68c25206b2df7217d",
+    "url": "/accents/static/css/main.747614b2.chunk.css"
+  },
+  {
+    "revision": "3b03f2f41301e4ec9cda",
+    "url": "/accents/static/js/2.cb7ebafd.chunk.js"
+  },
+  {
+    "revision": "5fd68c25206b2df7217d",
+    "url": "/accents/static/js/main.2b80c885.chunk.js"
+  },
+  {
+    "revision": "c473bb028dd3f6dfbd23",
+    "url": "/accents/static/js/runtime~main.1dccf40c.js"
+  }
+]);
